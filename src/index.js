d59b545f2b0a346197340d1843ba6a5eb34da6f9
play.js <git@gituser.com>
2021-10-08 18:41:11 -0400

    configured .html, .Jsx and, .css

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+ <img src="https://i.imgur.com/HSsvvXC.png"/>
+
+<h1>xINO Apparell</h1>
+
+<div>
+  <ul>
+  
+ <li>
+   Men
+     <select>
+       <option>
+         jeans
+       </option>
+     </select>
+   </li>
+   
+ <li>
+   Women
+     <select>
+       <option>
+         jeans
+       </option>
+     </select>
+   </li>
+   
+ <li>
+   Kids
+     <select>
+        <option>
+               <a href="https://www.google.com">Tops</a>
+         </option>
+         <option>
+               Bottoms
+         </option>
+     </select>
+   </li>
+ 
+  </ul>
+  </div>
+`
